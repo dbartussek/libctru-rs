fd253e42b92af307b077986fa26be5741498a92a
Dario Bartussek <d.bartussek@gmail.com>
2021-04-19 16:12:56 +0200

    Puts appends a newline

--- src/print_impl.rs
+++ src/print_impl.rs
@@ -2,6 +2,9 @@ use crate::raw::c_char;
 
 extern "C" {
     #[doc(hidden)]
+    pub fn printf(formatting: *const c_char, string: *const c_char);
+
+    #[doc(hidden)]
     pub fn puts(string: *const c_char);
 }
 
@@ -9,7 +12,8 @@ extern "C" {
 macro_rules! print {
     () => ();
     ($($arg:tt)*) => (unsafe {
-        $crate::print_impl::puts(
+        $crate::print_impl::printf(
+            "%s",
             $crate::alloc::format!(
                 "{}\0",
                 $crate::alloc::format!($($arg)*)
@@ -21,12 +25,12 @@ macro_rules! print {
 #[macro_export]
 macro_rules! println {
     () => (unsafe {
-        $crate::print_impl::puts("\n\0".as_ptr());
+        $crate::print_impl::puts("\0".as_ptr());
     });
     ($($arg:tt)*) => (unsafe {
         $crate::print_impl::puts(
             $crate::alloc::format!(
-                "{}\n\0",
+                "{}\0",
                 $crate::alloc::format!($($arg)*)).as_ptr()
         );
     });
